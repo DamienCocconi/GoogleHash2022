9e353dfed7beb26eef9ca2169ce92e522ffc8529
adeo20007757 <francois.messiaen@ext.adeo.com>
2022-02-24 19:55:56 +0100

    Update skills in contrib

--- src/main/java/fr/sioc1981/hashcode/y2022/pizzapuzzle/Contributor.java
+++ src/main/java/fr/sioc1981/hashcode/y2022/pizzapuzzle/Contributor.java
@@ -1,18 +1,18 @@
 package fr.sioc1981.hashcode.y2022.pizzapuzzle;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Contributor {
 	
 	String name;
-	
-	ArrayList<Skill> skills = new ArrayList<>();
-	
+
 	boolean available = true;
+	
+	HashMap<String, Skill> skills = new HashMap<>();
 
 	@Override
 	public String toString() {
-		return "Contributor [name=" + name + ", skills=" + skills + ", available=" + available + "]";
+		return "Contributor [name=" + name + ", available=" + available + ", skills=" + skills + "]";
 	}
-	
+
 }
