bf39dcd9a2a1f855c31e0499f6980c2142af5e86
Damien Cocconi <damien.cocconi@ext.adeo.com>
2022-02-24 19:07:25 +0100

    first commit

---
+++ src/data/Contributor.java
@@ -0,0 +1,11 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Contributor {
+	
+	String name;
+	
+	ArrayList<Skill> skills;
+
+}
---
+++ src/data/Project.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Project {
+	
+	int score;
+	
+	int duration;
+	
+	int bestBefore;
+	
+	ArrayList<Skill> skills;
+
+}
