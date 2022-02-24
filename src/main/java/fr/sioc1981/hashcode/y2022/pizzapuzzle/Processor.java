e9f3000a2b16b177ee6a2f8b7bbd45b782d0f104
Damien Cocconi <damien.cocconi@ext.adeo.com>
2022-02-24 20:42:42 +0100

    new processor implementation

--- src/main/java/fr/sioc1981/hashcode/y2022/pizzapuzzle/Processor.java
+++ src/main/java/fr/sioc1981/hashcode/y2022/pizzapuzzle/Processor.java
@@ -2,31 +2,52 @@ package fr.sioc1981.hashcode.y2022.pizzapuzzle;
 
 public class Processor {
 	
-	int currentDay = 0;
+	static int currentDay = 0;
 	
-	public void process(Launcher launcher) {
+	public static void process(Launcher launcher) {
 		
-		while(true) {
 			
 			//parcourir les projets dispo
 			for(int i = 0; i < launcher.AVAILABLE_PROJECTS.size(); i++) {
 				Project project = launcher.AVAILABLE_PROJECTS.get(i);
+				boolean ready = true;
 				
 				//trouver des contributeurs disponibles qui correspondent aux skills demandés
 				for(Skill requiredSkill : project.skills) {
-					boolean contributorFound = false;
+					Contributor contrib = null;
+					
 					for(Contributor contributor : launcher.CONTRIBUTORS) {
+						if(!contributor.available)
+							continue;
+						
 						Skill contributorSkill = contributor.skills.get(requiredSkill.name);
-						if(contributorSkill != null && contributorSkill.level >= requiredSkill.level )
+						if(contributorSkill != null && contributorSkill.level >= requiredSkill.level ) {
 							//attribuer le contributeur
-							
-							project.contributors.add(contributor);
+							contrib = contributor;
+							break;
+						}
+
+					}
+					if(contrib != null) {
+						project.contributors.add(contrib);
+					}else {
+						ready = false;
+						project.contributors.clear();
+						break;
 					}
 				}
+				
+				if(ready) {
+					project.contributors.forEach(c -> c.available = false);
+					launcher.PROJECTS_RELEASED.add(project);
+					//launcher.AVAILABLE_PROJECTS.remove(project)
+				}else {
+					//nothing to do for now
+				}
 			}
+			
+			launcher.AVAILABLE_PROJECTS.removeAll(launcher.PROJECTS_RELEASED);
 
-			currentDay++;
-		}
 	}
 
 }
